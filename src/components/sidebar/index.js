652f10bbe12b5cc6e0d284f634d2f7797e9eccb6
Sid Karthik <sa268592@gmail.com>
2021-08-20 17:19:58 +0530

    Changes 1

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -2,11 +2,14 @@ import InfoCard from "./InfoCard"
 import UserCard from "./UserCard"
 import Button from "@material-tailwind/react/Button";
 import Icon from "@material-tailwind/react/Icon";
+import * as ROUTES from "../../constants/routes";
+import { Link } from "react-router-dom";
+
 
 function Sidebar() {
     return (
       <div className="hidden h-screen pt-5 md:inline-flex flex-col space-y-4 sticky">
-        <div className="flex justify-end border-b mb-5 ">
+        <div className="flex justify-between border-b mb-5 ">
           <Button
             color="blueGray"
             buttonType="link"
@@ -19,6 +22,20 @@ function Sidebar() {
           >
             <Icon name="notifications" size="regular" className="w-20 h-20" />
           </Button>
+          <Link to={ROUTES.LOGIN} >
+            <Button
+              color="blue"
+              buttonType="filled"
+              size="large"
+              rounded={false}
+              block={false}
+              iconOnly={false}
+              ripple="light"
+              className="flex rounded-xl my-5"
+            >
+              Login
+            </Button>
+          </Link>
         </div>
         <UserCard />
         <InfoCard />
